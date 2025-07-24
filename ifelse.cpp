c649d2b90fbeb5176ed90fbdbeae948edea04f59
vonirise <masumifelix7@gmail.com>
2025-07-25 00:24:58 +0800

    penambahan ifelse

--- ifelse.cpp
+++ ifelse.cpp
@@ -5,4 +5,17 @@ int main () {
     int a, b;
     cout << "Masukkan dua bilangan: ";
     
+    if (cin >> a >> b) {
+        
+        if (a > b) {
+            cout << "Bilangan pertama lebih besar dari bilangan kedua." << endl;
+        } else if (a < b) {
+            cout << "Bilangan pertama lebih kecil dari bilangan kedua." << endl;
+        } else {
+            cout << "Kedua bilangan sama." << endl;
+        }
+    } else {
+        cout << "Input tidak valid." << endl;    
+    return 0;
+}
     }
